84d24a9815b2584b7e42b8da89a24eab5c5b28e1
cyrus998 <jonathanfranciscosopro@gmail.com>
2022-10-06 14:25:34 +0800

    pog2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,8 +25,8 @@ return new class extends Migration
             $table->string('address')->nullable();
             $table->string('aboutme')->nullable();
             $table->string('subjectexpertise')->nullable();
-            $table->string('image')->nullable();
-
+            $table->string('image')->default('public/portfolios/nQj3sigByW0nClBcywKF3RHog92pjoInfRwJtMGc.png');
+            
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->rememberToken();
