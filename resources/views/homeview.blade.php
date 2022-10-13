5cef67faf937d9fafb759ea413b793be057a695a
unknown <milfllabres@tip.edu.ph>
2022-10-13 20:38:59 +0800

    nav

--- resources/views/homeview.blade.php
+++ resources/views/homeview.blade.php
@@ -68,7 +68,7 @@
                     </a>
 
                 </ul>
-                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                <button class="ml-5 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                     <a href="/portal" style="color: white;" href="" :active="request() - > routeIs('/')">
                         {{ __('Faculty Portal') }}
                     </a>
