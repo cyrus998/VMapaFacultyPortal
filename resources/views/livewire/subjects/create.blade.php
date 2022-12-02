ebe4316fee30d1842511fe0afb5fc96de7297b93
cyrus998 <jonathanfranciscosopro@gmail.com>
2022-12-03 00:18:03 +0800

    fixed user issue, adjusted other views, commented the extra nav link

--- resources/views/livewire/subjects/create.blade.php
+++ resources/views/livewire/subjects/create.blade.php
@@ -11,59 +11,46 @@
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
 
         <div class="mt-2 inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-            <form>
-                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div class="">
-                        <div class="mb-4">
-                            <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">Subject / Course Name:</label>
-                            <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Enter Subject / Course Name" wire:model="subjectname">
-                            @error('subjectname')
-                            <span class="text-red-500">{{ $message }}</span>
-                            @enderror
-                        </div>
+            <div class="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+                <form action="" class="space-y-4">
+                    <div>
+                        <label class="sr-only" for="name">Name</label>
+                        <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Enter Subject / Course Name" wire:model="subjectname">
+                        @error('subjectname')
+                        <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
 
-                        <div class="mb-4">
-                            <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">Course Code:</label>
+                    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                        <div>
+                            <label class="sr-only" for="coursecode">Course Code</label>
                             <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Course Code" wire:model="coursecode">
                             @error('coursecode')
                             <span class="text-red-500">{{ $message }}</span>
                             @enderror
                         </div>
-                     
-                        <div class="mb-4">
-                            <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">Section:</label>
+
+                        <div>
+                            <label class="sr-only" for="section">Section</label>
                             <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Section" wire:model="section">
                             @error('section')
                             <span class="text-red-500">{{ $message }}</span>
                             @enderror
                         </div>
+                    </div>
 
-                        <div class="mb-4">
-                            <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">Room No:</label>
+
+                    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                        <div>
+                            <label class="sr-only" for="roomno">Room No:</label>
                             <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Room Number" wire:model="roomno">
                             @error('roomno')
                             <span class="text-red-500">{{ $message }}</span>
                             @enderror
                         </div>
 
-                        <div class="mb-4">
-                            <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">Name of Instructor:</label>
-
-                            <select wire:model="instructor" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1">
-
-                                <option value="">-- Select an Instructor --</option>
-                                @foreach (App\Models\User::where('position', 'teacher')->get() as $user)
-                                <option value="{{ $user->name }}">{{ $user->name }}</option>
-                                @endforeach
-                            </select>
-                            @error('instructor')
-                            <span class="text-red-500">{{ $message }}</span>
-                            @enderror
-                        </div>
-
-                        <div class="mb-4">
-                            <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">Day of the Subject:</label>
-
+                        <div>
+                            <label class="sr-only" for="section">Day of Subject</label>
                             <select wire:model="subjectday" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1">
 
                                 <option value="">-- Select A Day --</option>
@@ -73,46 +60,61 @@
                                 <option value="Thursday">Thursday</option>
                                 <option value="Friday">Friday</option>
                                 <option value="Saturday">Saturday</option>
-        
+
                             </select>
                             @error('subjectday')
                             <span class="text-red-500">{{ $message }}</span>
                             @enderror
-
                         </div>
-               
-                        <div class="mb-4">
-                            <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">Start time:</label>
-                            <input type="time" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Start Time" wire:model="starttime">
-                            @error('starttime')
-                            <span class="text-red-500">{{ $message }}</span>
-                            @enderror
-                        </div>
-
-                        <div class="mb-4">
-                            <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">End time:</label>
-                            <input type="time" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="End Time" wire:model="endtime">
-                            @error('endtime')
-                            <span class="text-red-500">{{ $message }}</span>
-                            @enderror
-                        </div>
-
                     </div>
-                </div>
 
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-                        <button wire:click.prevent="store()" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                            Save
-                        </button>
-                    </span>
-                    <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+                    <div>
+                        <label class="sr-only" for="instructorname">Name of Instructor:</label>
+                        <select wire:model="instructor" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1">
 
-                        <button wire:click="closeModal()" type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                            Cancel
-                        </button>
-                    </span>
-            </form>
+                            <option value="">-- Select an Instructor --</option>
+                            @foreach (App\Models\User::where('position', 'teacher')->get() as $user)
+                            <option value="{{ $user->name }}">{{ $user->name }}</option>
+                            @endforeach
+                        </select>
+                        @error('instructor')
+                        <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+
+                    <div class="mb-4">
+                        <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">Start time:</label>
+                        <input type="time" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Start Time" wire:model="starttime">
+                        @error('starttime')
+                        <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="exampleFormControlInput1" class="block text-gray-700 text-sm font-bold mb-2">End time:</label>
+                        <input type="time" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="End Time" wire:model="endtime">
+                        @error('endtime')
+                        <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class=" px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+                            <button wire:click.prevent="store()" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                                Save
+                            </button>
+                        </span>
+                        <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+
+                            <button wire:click="closeModal()" type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                                Cancel
+                            </button>
+                        </span>
+                    </div>
+
+                </form>
+            </div>
         </div>
 
     </div>
--- resources/views/livewire/users/create.blade.php
+++ resources/views/livewire/users/create.blade.php
@@ -1,19 +1,15 @@
-{{-- second modal --}}
-@if (Auth::check())
-    @if (Auth::user()->role == '1')
-        <div class="fixed z-10 inset-0 overflow-y-auto ease-out duration-400">
-            <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+<div class="fixed z-10 inset-0 overflow-y-auto ease-out duration-400">
+  <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
-                <div class="fixed inset-0 transition-opacity">
-                    <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-                </div>
+    <div class="fixed inset-0 transition-opacity">
+      <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+    </div>
 
-                <!-- This element is to trick the browser into centering the modal contents. -->
-                <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
-                {{-- //$name, $email, $password, $facultyNumber, $role, $position --}}
+    <!-- This element is to trick the browser into centering the modal contents. -->
+    <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>​
 
-
-                <div class="mt-5 flex w-full max-w-sm mx-auto bg-white rounded-lg dark:bg-gray-800 lg:max-w-4xl overflow-hidden shadow-xl transform transition-all"
+    <div class="inline-block align-bottom rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-full sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+    <div class="mt-5 flex w-full max-w-sm mx-auto bg-white rounded-lg dark:bg-gray-800 lg:max-w-4xl overflow-hidden shadow-xl transform transition-all"
                     role="dialog" aria-modal="true" aria-labelledby="modal-headline">
                     <div class="hidden bg-cover lg:block lg:w-1/2"
                         style="background-image: url('https://images.unsplash.com/photo-1598379232411-d670d6872193?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80');">
@@ -106,42 +102,8 @@
 
                     </div>
                 </div>
+    </div>
 
-            </div>
-        </div>
-        </div>
-        </div>
-    @endif
-    @if (Auth::user()->role == '0')
-        <div class="flex items-center justify-between p-4 text-red-700 border rounded gap-4 border-red-900/10 bg-red-50"
-            role="alert">
-            <div class="flex items-center gap-4">
-                <span class="p-2 text-white bg-red-600 rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M20.618 5.984A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016zM12 9v2m0 4h.01" />
-                    </svg>
-                </span>
-
-                <p>
-                    <strong class="text-sm font-medium"> Uh-oh! </strong>
-
-                    <span class="block text-xs opacity-90">
-                        You do not have Admin Priviledges to access this page.
-                    </span>
-                </p>
-            </div>
-
-            <button class="opacity-90" type="button">
-                <span class="sr-only"> Close </span>
-
-                <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clip-rule="evenodd" />
-                </svg>
-            </button>
-        </div>
-    @endif
-@endif
+  </div>
+</div>
+</div>
